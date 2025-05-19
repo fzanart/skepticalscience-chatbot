0cbd09dc740e189d2e71ef7d1e24d32a970fb829
Francisco Zanartu <fzanartu@users.noreply.huggingface.co>
2025-05-19 01:36:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from huggingface_hub import InferenceClient
 """
 For more information on `huggingface_hub` Inference API support, please check the docs: https://huggingface.co/docs/huggingface_hub/v0.22.2/en/guides/inference
 """
-client = InferenceClient("HuggingFaceH4/zephyr-7b-beta")
+client = InferenceClient(model="prompthero/openjourney-v4")
 
 
 def respond(
@@ -45,18 +45,18 @@ For information on how to customize the ChatInterface, peruse the gradio docs: h
 """
 demo = gr.ChatInterface(
     respond,
-    additional_inputs=[
-        gr.Textbox(value="You are a friendly Chatbot.", label="System message"),
-        gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
-        gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
-        gr.Slider(
-            minimum=0.1,
-            maximum=1.0,
-            value=0.95,
-            step=0.05,
-            label="Top-p (nucleus sampling)",
-        ),
-    ],
+    # additional_inputs=[
+    #     gr.Textbox(value="You are a friendly Chatbot.", label="System message"),
+    #     gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
+    #     gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
+    #     gr.Slider(
+    #         minimum=0.1,
+    #         maximum=1.0,
+    #         value=0.95,
+    #         step=0.05,
+    #         label="Top-p (nucleus sampling)",
+    #     ),
+    # ],
 )
 
 
