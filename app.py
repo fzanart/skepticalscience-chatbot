e75eaab532bd3b9990e0e42e0eafbd3f035b7f48
Francisco Zanartu <fzanartu@users.noreply.huggingface.co>
2025-05-19 18:11:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 import random
 import gradio as gr
+from langchain_openai import ChatOpenAI
 
-def random_response(message, history):
-    return random.choice(["Yes", "No"])
 
-demo = gr.ChatInterface(random_response, type="messages", autofocus=False)
+
+model = ChatOpenAI(
+    temperature=0,
+    openai_api_key=os.environ.get("OPENAI_API_KEY"),
+    openai_organization=os.environ.get("OPENAI_ORGANIZATION_ID"),
+    model_name="gpt-4o",
+)
+
+
+
+def simple_response(message):
+
+    response = model.invoke(message)
+    
+    return response.content
+
+demo = gr.ChatInterface(simple_response, type="messages", autofocus=False)
 
 if __name__ == "__main__":
     demo.launch()
